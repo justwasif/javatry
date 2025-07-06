09f9db47e02875c1b452ac5a39b7a7b1baf63698
justwasif <wasif.iit.ansari@gmail.com>
2025-07-06 10:49:45 +0000

    L-12 tutorial complete

---
+++ my file/l12.js
@@ -0,0 +1,14 @@
+const no = 594
+const nn = new Number(594)
+console.log(nn);
+console.log(no);
+console.log(nn.toString().length);
+console.log(nn.toFixed(2)); ///for decimal 
+const dec =123.443
+console.log(dec.toPrecision(3));
+const ind = 1000000
+console.log(ind.toLocaleString("en-IN"));
+console.log(Math);
+console.log(Math.abs(-4));///for abslute value
+console.log(Math.round(6.5));
+console.log(Math.ceil(1.1)); ///same for floor
