e6d683c303d0566db808ee0b2f1c5b6a1f620c90
justwasif <wasif.iit.ansari@gmail.com>
2025-07-06 19:22:28 +0000

    feat:added date and time

--- my file/date.js
+++ my file/date.js
@@ -1,9 +1,9 @@
-///let mydate = new Date()
-///console.log(mydate.toString());
-//let me = new Date(2025,9,5)
-//console.log(me.toString());
-//let met = new Date("05-10-2006")
-//console.log(met.toLocaleString());
+let mydate = new Date()
+console.log(mydate.toString());
+let me = new Date(2025,9,5)
+console.log(me.toString());
+let met = new Date("05-10-2006")
+console.log(met.toLocaleString());
 let stamp = Date.now()
 let thi = stamp/1000
 console.log(Math.round(thi));
