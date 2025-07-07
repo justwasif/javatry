71590cbf2147f057e6b08742619d3f2f79b6a309
justwasif <wasif.iit.ansari@gmail.com>
2025-07-07 18:56:48 +0000

    feat:arry completed

--- my file/arry.js
+++ my file/arry.js
@@ -20,3 +20,14 @@ console.log(arr);
  let motdiff = arr.splice(1,3)
  console.log(motdiff);
  console.log("B",arr);
+
+let no1 =[1,2,3,4,5,6]
+let no2 = [7,8,9,0]
+no1.push(no2)
+let  no3 = no1.concat(no2)
+console.log(no3);
+//spred 
+const b =[...no1,...no2]
+console.log(b);
+let merg = no1.flat(Infinity)
+console.log(merg);
---
+++ my file/object.js
@@ -0,0 +1,4 @@
+//literals object
+let ob = {NAME:"WASIF",age:18,"roll":"nine"}
+console.log(ob.NAME);
+console.log(ob["roll"]);
