2e359c4a1a89f944e8f1e24bf4f0e1fede0024ab
justwasif <wasif.iit.ansari@gmail.com>
2025-07-09 14:37:27 +0000

    feat:function lecture comp

--- my file/parameters.js
+++ my file/parameters.js
@@ -3,11 +3,29 @@
 // my()
 // function cal(no1,no2){console.log(no1+no2);}
 // cal(10,12)
-function user(username){
-    if(username === undefined){
-        console.log("entre username")
-        return
-    }
-    return `${username} just logged in`; //backtick use karna hota hai single quote nahi
+// function user(username){
+//     if(username === undefined){
+//         console.log("entre username")
+//         return
+//     }
+//     return `${username} just logged in`; //backtick use karna hota hai single quote nahi
+// }
+// console.log(user());
+// function add(mu1,mu2,...nu){
+//     return [nu,mu1,mu2]
+
+// }
+// console.log(add(200,300,50,400));
+// const see ={username:"wasif",price:400}
+
+// function objectspicific(anyobject){
+//     console.log(`username ${anyobject.username} and price ${anyobject.price}`);
+
+//     }
+// objectspicific(see)
+
+
+function arry(get){
+    return get[1]
 }
-console.log(user());
+console.log(arry([200,210,123,111]));
