2b7f70089631ce7e76a8afa75b710e48b5be54e3
justwasif <wasif.iit.ansari@gmail.com>
2025-07-10 21:08:44 +0000

    feat:arrow fxn

---
+++ my file/arrow.js
@@ -0,0 +1,26 @@
+// const user={
+//     username:"wasif",
+//     price:099,
+//     welcome:function(){
+//         console.log(`${this.username},welcome`);
+//     }
+// }
+// user.welcome()
+// user.username = "talha"
+// user.welcome()
+// console.log(this);
+// function bad(){
+//     console.log(this);
+// }
+// bad()
+
+// arro fxn ()=> {}
+    const add = (no1,no2)=> { 
+           return no1 + no2
+    }
+console.log(add(4,4));
+// without return 
+const nw = (nu1,nu2)=>(nu1+nu2)
+console.log(nw(3,3));
+
+
--- my file/scope.js
+++ my file/scope.js
@@ -1,6 +1,18 @@
-if (true){
-    var c =10
-    const a =20
-    let n = 30
+// if (true){
+//     var c =10
+//     const a =20
+//     let n = 30
+// }
+// console.log(c)
+
+function one(){
+    const st = "main"
+    console.log(st);
+    function two(){
+        const nd ="sub"
+        console.log(nd)
+    }
+    console.log(nd)
+    two()
 }
-console.log(c)
+console.log(one)
