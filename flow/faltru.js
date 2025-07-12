ce56ba3a586cfba7de4b9ef3d5a8cce4f13ebd5b
justwasif <wasif.iit.ansari@gmail.com>
2025-07-12 13:32:47 +0000

    feat: practiced conditional statements and switch-case

---
+++ flow/faltru.js
@@ -0,0 +1,9 @@
+let use =""
+if(use){
+    console.log("yes user");
+}else{
+    console.log(NaN)
+}
+let nn;
+nn= null ?? 15
+console.log(nn);
--- flow/flow.js
+++ flow/flow.js
@@ -1,19 +1,27 @@
-// const balance = 1000
-// if(balance>500){
-//     return console.log("test");
-// }
-// console.log(balance);
-// const use =true
-// const cars = true
-// if(use && card){
-//     console.log("allow");
-// }
+const balance = 1000
+if(balance>500){
+    return console.log("test");
+}
+console.log(balance);
+const use =true
+const cars = true
+if(use && card){
+    console.log("allow");
+}
 const month =3
 switch(month){
     case 1:
         console.log("JAN")
         break;
     case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    default:
+        console.log("not avabilivil");
+        break;
         
 
 }
