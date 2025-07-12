c05b8066546fea70263a06c11ca6d571a49a6c5e
justwasif <wasif.iit.ansari@gmail.com>
2025-07-12 20:15:21 +0000

    feat:while,do loop

---
+++ flow/while.js
@@ -0,0 +1,16 @@
+let i = 0
+while(i<10){
+    console.log(`value of i is ${i}`);
+    i = i +1
+}
+let arroe =0
+let arr =["me","i","know"]
+while(arroe<arr.length){
+    console.log(`value is ${arr[arroe]}`);
+    arroe=arroe+1
+}
+let st =1
+do{
+    console.log(`score is ${st}`);
+    st= st+1
+}while (st<11);
