859366cc0683eed6b8f38a1df60a36f1e6dd8d50
justwasif <wasif.iit.ansari@gmail.com>
2025-07-12 09:04:26 +0000

    feat:flow

---
+++ flow/flow.js
@@ -0,0 +1,19 @@
+// const balance = 1000
+// if(balance>500){
+//     return console.log("test");
+// }
+// console.log(balance);
+// const use =true
+// const cars = true
+// if(use && card){
+//     console.log("allow");
+// }
+const month =3
+switch(month){
+    case 1:
+        console.log("JAN")
+        break;
+    case 2:
+        
+
+}
