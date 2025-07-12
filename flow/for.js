ea9e0dcfdf98f3b1bad76f803b4d7b71842b575d
justwasif <wasif.iit.ansari@gmail.com>
2025-07-12 17:13:19 +0000

    loop

---
+++ flow/for.js
@@ -0,0 +1,25 @@
+// console.log("hi")
+// for (let i = 0; i < 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 0; j <10; j++) {
+//         console.log(`inner loop ${j} and inner loop ${i}`);
+        
+//     }
+
+    
+//}
+// let arr =["marvel","dc","diseny"]
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+// 
+
+for (let i = 0; i < 21; i++) {
+    if(i==5){
+        console.log("got 5" );
+        continue;
+    }
+    console.log(`value of i ${i}`);
+    
+}
